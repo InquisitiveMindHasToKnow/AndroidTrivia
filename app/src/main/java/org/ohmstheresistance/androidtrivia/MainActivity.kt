4566edc4eee860411fc07b19c41a95c7acea5660
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2020-02-09 20:54:49 -0500

    Added necessary fragments and layouts for trivia.

--- app/src/main/java/org/ohmstheresistance/androidtrivia/MainActivity.kt
+++ app/src/main/java/org/ohmstheresistance/androidtrivia/MainActivity.kt
@@ -2,11 +2,17 @@ package org.ohmstheresistance.androidtrivia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import org.ohmstheresistance.androidtrivia.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            @Suppress("UNUSED_VARIABLE")
+
+            val binding = DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.main_activity)
+        }
     }
 }
---
+++ app/src/main/java/org/ohmstheresistance/androidtrivia/fragments/GameFragment.kt
@@ -0,0 +1,21 @@
+package org.ohmstheresistance.androidtrivia.fragments
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import org.ohmstheresistance.androidtrivia.R
+
+class GameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.game_fragment, container, false)
+    }
+
+
+}
