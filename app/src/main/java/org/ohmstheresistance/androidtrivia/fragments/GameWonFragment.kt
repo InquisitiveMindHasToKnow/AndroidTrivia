d6eb1b43ae429d6c1e03df65b574bbe9396ad450
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2020-02-10 18:37:34 -0500

    Used ShareCompat to implicitly share winning info with other applications if user needs.

--- app/src/main/java/org/ohmstheresistance/androidtrivia/fragments/GameWonFragment.kt
+++ app/src/main/java/org/ohmstheresistance/androidtrivia/fragments/GameWonFragment.kt
@@ -1,14 +1,15 @@
 package org.ohmstheresistance.androidtrivia.fragments
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 import org.ohmstheresistance.androidtrivia.R
 import org.ohmstheresistance.androidtrivia.databinding.GameWonFragmentBinding
@@ -23,14 +24,46 @@ class GameWonFragment : Fragment() {
 
             //view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
             view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
-
         }
 
-        var winningInfoArgs = arguments?.let { GameWonFragmentArgs.fromBundle(it) }
-        Toast.makeText(context, "Number Correct: ${winningInfoArgs!!.numCorrect}, Total:${winningInfoArgs.numQuestions}", Toast.LENGTH_SHORT).show()
+        setHasOptionsMenu(true)
 
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+
+        if (null == getShareIntent()?.resolveActivity(activity!!.packageManager)) {
+            menu.findItem(R.id.share)?.setVisible(false)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun getShareIntent(): Intent? {
+
+        var winningInfoArgs = arguments?.let { GameWonFragmentArgs.fromBundle(it) }
+
+        return activity?.let {
+            ShareCompat.IntentBuilder.from(it)
+                .setText(getString(R.string.share_success_text,
+                    winningInfoArgs?.numCorrect, winningInfoArgs?.numQuestions
+                ))
+                .setType("text/plain")
+                .intent
+        }
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
 
 }
