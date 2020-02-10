e6afeab06d47b9a10b78300ae7717a0beaffaf7b
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2020-02-10 13:49:50 -0500

    Added an options menu to the title page to tell users about the app.

--- app/src/main/java/org/ohmstheresistance/androidtrivia/fragments/GameOverFragment.kt
+++ app/src/main/java/org/ohmstheresistance/androidtrivia/fragments/GameOverFragment.kt
@@ -19,7 +19,7 @@ class GameOverFragment : Fragment() {
         val binding: GameOverFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.game_over_fragment, container, false)
 
         binding.tryAgainButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_gameOverFragment2_to_gameFragment)
+            view.findNavController().navigate(R.id.action_gameOverFragment_to_gameFragment)
         }
 
             return binding.root
